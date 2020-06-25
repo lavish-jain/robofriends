54a47ce3e7145f587005bbfc230d647ac85bd817
lavish-jain <lavishrjain1997@gmail.com>
2020-06-25 13:28:39 +0530

    Added Loading Component. Changed data from in built object to jsonplaceholder api call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import './App.css';
-import { robots } from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Loading from './Loading';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: '',
         };
     }
-    onSearchChange = event => {
-        this.setState({ searchField: event.target.value })
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+            this.setState({ robots: response.data })
+
+        })
     }
     render() {
         const filteredRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()));
+        if (this.state.robots.length === 0) {
+            return (
+                <Loading />
+            );
+        }
         return (
             <div className='tc'>
                 <h1 className='f1'>Robofriends</h1>
@@ -24,6 +33,9 @@ class App extends React.Component {
                 <CardList robots={filteredRobots} />
             </div>
         )
+    }
+    onSearchChange = event => {
+        this.setState({ searchField: event.target.value })
     }
 }
 
